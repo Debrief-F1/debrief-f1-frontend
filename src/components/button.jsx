ad8589182d51ec3f3d2b49b539f444bc047acfb2
Mohammed SLIMANI <mslimani62@yahoo.fr>
2022-07-01 15:51:17 +0200

    button

---
+++ src/components/button.jsx
@@ -0,0 +1,4 @@
+const Button = () => {
+  return <button>ADD</button>
+}
+export default Button
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,9 @@
+
 const IndexPage = () => {
-  return <h1 className="p-4 bg-green-600">Hello</h1>
+
+  return <div className="flex gap-4">
+    <div className="p-4 bg-green-600 ">Hello </div>
+    <div className="bg-red-600">gooooo</div>
+  </div>
 }
 export default IndexPage;
