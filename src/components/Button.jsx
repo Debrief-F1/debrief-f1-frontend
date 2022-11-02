1744dd440ae2a0fb51598141801da1d8e8d3c119
Kevin GY <k.gy@laposte.net>
2022-11-02 14:50:39 +0100

    first commit Kevin

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,6 +4,7 @@ const Button = (props) => {
 
   return (
     <button
+    // blabla
       {...otherProps}
       className={classNames(
         "px-3 py-2 font-lg font-bold text-white bg-blue-600 active:bg-blue-700 disabled:bg-blue-500 border-4 border-blue-600 active:border-blue-700 disabled:border-blue-500 rounded-sm",
